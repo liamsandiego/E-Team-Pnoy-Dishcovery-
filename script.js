e40a91d24f97ced864bb2d62946de6a35aa10911
Kaels10 <118781070+Kaels10@users.noreply.github.com>
2024-04-27 17:53:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function searchRecipe() {
+    var searchInput = document.getElementById('search-input').value;
+    // Perform your search logic here, e.g., redirect to search results page
+    // or filter the recipes based on the search input.
+  }
+  
